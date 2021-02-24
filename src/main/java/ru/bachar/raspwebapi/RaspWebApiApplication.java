dedc60aab7463fd2b927a29880011333140c5576
ibacharnikov <ibacharnikov@cinimex.ru>
2021-02-24 17:06:02 +0500

    Init commit

---
+++ src/main/java/ru/bachar/raspwebapi/RaspWebApiApplication.java
@@ -0,0 +1,13 @@
+package ru.bachar.raspwebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaspWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaspWebApiApplication.class, args);
+	}
+
+}
