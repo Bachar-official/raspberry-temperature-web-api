b5e80d7f840ac604a09ed9915028f13184bf8567
ibacharnikov <ibacharnikov@cinimex.ru>
2021-02-24 17:50:55 +0500

    Main entity. Commented call of GPIO.

--- src/main/java/ru/bachar/piwebapi/Measure/Measure.java
+++ src/main/java/ru/bachar/piwebapi/Measure/Measure.java
@@ -9,4 +9,70 @@ import javax.persistence.Id;
 @Entity
 public class Measure {
     private @Id @GeneratedValue Long id;
+    private String date;
+    private double temperature;
+    private double humidity;
+
+    Measure() {}
+
+    Measure(String date, double temperature, double humidity) {
+        this.date = date;
+        this.temperature = temperature;
+        this.humidity = humidity;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getDate() {
+        return this.date;
+    }
+
+    public double getTemperature() {
+        return this.temperature;
+    }
+
+    public double getHumidity() {
+        return this.humidity;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public void setHumidity(double humidity) {
+        this.humidity = humidity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Measure)) {
+            return false;
+        }
+        Measure m = (Measure) o;
+        return this.equals(m);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.id, this.date, this.temperature, this.humidity);
+    }
+
+    @Override
+    public String toString() {
+        return "Measure number " + this.id.toString() + " (" + this.date + ")\n" +
+        "temperature: " + this.temperature + ", humidity: " + this.humidity + "\n"; 
+    }
 }
--- src/main/java/ru/bachar/piwebapi/PiWebApiApplication.java
+++ src/main/java/ru/bachar/piwebapi/PiWebApiApplication.java
@@ -11,3 +11,24 @@ public class PiWebApiApplication {
 	}
 
 }
+
+/*
+* From main java application
+private static final int DHT_WAIT_INTERVAL = 2000;
+DHTxx dht22 = new DHT22(RaspiPin.GPIO_07);
+        System.out.println(dht22);
+
+        try {
+            dht22.init();
+            for (int i = 0; i < 10; i++) {
+                try {
+                    System.out.println(dht22.getData());
+                    Thread.sleep(DHT_WAIT_INTERVAL);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
+*/
